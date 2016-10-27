bd822c56877eb152d58b22954edc69025ba638f4
lespoir <stiven140@hotmail.com>
2016-10-27 14:55:13 +0000

    added animations

--- js/AIO/index.js
+++ js/AIO/index.js
@@ -1,5 +1,29 @@
 
+var typed = false;
 $( document ).ready(function() {
     var visualModule = new VisualModule();
     visualModule.init(visualModule);
+    AOS.init();
+    
+    $(window).scroll(function() {    
+        if(typed == false && isScrolledIntoView($('#intro')))
+        {
+            typed = true;
+            $('#pregunta_generadora').typeIt({
+                 speed: 50,
+                 autoStart: false,
+                 cursor:false,
+            })
+            .tiType('¿Cómo debe ser la <span class="span_h2"> arquitectura lógica y física </span> de una plataforma robótica enfocada a la <span class="span_h2"> dramatización </span>?');
+        }    
+    });
 });
+
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+    return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
